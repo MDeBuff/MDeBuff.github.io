abdbf465fc31bb89e79d278b6845b3a46f416c96
MDeBuff <michael.debuff@umontana.edu>
2019-11-07 21:30:52 -0700

    Updates

---
+++ precache-manifest.311c967bdd7a2df4b4636cbe037adccd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec05829cbf585ceb8b17c28fdcf6d104",
+    "url": "/ccapp/index.html"
+  },
+  {
+    "revision": "32750ddbfab2ae417865",
+    "url": "/ccapp/static/css/main.05385729.chunk.css"
+  },
+  {
+    "revision": "61f589d009500cc57e59",
+    "url": "/ccapp/static/js/2.e5b4f29a.chunk.js"
+  },
+  {
+    "revision": "32750ddbfab2ae417865",
+    "url": "/ccapp/static/js/main.2326f882.chunk.js"
+  },
+  {
+    "revision": "34461c36026ef38bf631",
+    "url": "/ccapp/static/js/runtime-main.1dd474b8.js"
+  },
+  {
+    "revision": "5eb5baa77b106e4c547553bde0d6061b",
+    "url": "/ccapp/static/media/CCLogo.5eb5baa7.svg"
+  }
+]);
